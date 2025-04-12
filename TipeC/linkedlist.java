840a709a565166c3726312f74b57d30404631ce6
klyroe <keylaanmrr@gmail.com>
2025-04-12 22:56:44 +0700

    tipe c

--- TipeC/linkedlist.java
+++ TipeC/linkedlist.java
@@ -1,5 +1,70 @@
 package TipeC;
 
 public class linkedlist {
+    private node head;
+
+    public void addMemb(member name){
+        node pendatang = new node(name);
     
+        if (head == null) {
+            head = pendatang;
+        } else {
+            node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = pendatang;
+        }
+    }
+    
+
+    public void delete(String name){
+        if (head == null) return; 
+
+        if (head.data.getNama().equals(name)) {
+            head = head.next;
+            return;
+        }
+        
+        node current = head;
+        while (current.next != null){
+            current.next = current.next.next;
+        }
+    }
+
+    public void search(String name){
+        node current = head;
+
+        while (current!= null) {
+            if (current.data.getNama().equalsIgnoreCase(name)) {
+                System.out.println(current.data);
+            }
+
+            current = current.next;
+        }
+    }
+
+    public void print(){
+        node current = head;
+
+        while (current != null) {
+            System.out.println(current.data + "\n");
+            current = current.next;
+        }
+    }
+
+
+    public void period(int tahun){
+        node current = head;
+
+        while (current!= null) {
+            if (current.data.getLamaJadiMember() > tahun) {
+                System.out.println(current.data);
+            }
+
+        current = current.next;
+        }
+
+    }
+
 }
--- TipeC/main.java
+++ TipeC/main.java
@@ -1,5 +1,40 @@
 package TipeC;
+import java.util.Scanner;
 
 public class main {
-    
+    public static void main(String[] args) {
+        linkedlist memblaundry = new linkedlist();
+        Scanner hi = new Scanner(System.in);
+
+        memblaundry.addMemb(new member("kei", 0001, "tralala", "Gold", 5));
+        memblaundry.addMemb(new member("markie", 0010, "tralele", "Platinum", 10));
+        memblaundry.addMemb(new member("haechi", 0027, "trralelo", "Silver", 9));
+
+        System.out.println("list member laundry xiangling: ");
+        memblaundry.print();
+
+        
+        System.out.println("final boss males nyuci");
+        memblaundry.period(10);
+        
+        memblaundry.print();
+
+        System.out.println("pilih: \n1. cari orang \n2. hapus nama");
+        int pilih = hi.nextInt();
+
+        
+        System.out.println("cari siapa: ");
+        String orang = hi.nextLine();
+        memblaundry.search(orang);
+            
+        System.out.println("hapuskan tentangku dari list\n mark/ haechan/ kei");
+        String villain = hi.nextLine();
+        memblaundry.delete(villain);
+        
+        
+
+        System.out.println("current list ada: ");
+        memblaundry.print();
+
+    }
 }
