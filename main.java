e5200e2586dd84380bab3ee052eb9f3ee62f8df3
klyroe <keylaanmrr@gmail.com>
2025-05-08 10:31:20 +0700

    Jurnal 11

--- main.java
+++ main.java
@@ -1,6 +1,27 @@
 public class main {
     
     public static void main(String[] args) {
-        topologic graph = new topologic();
+        topologic graf = new topologic();
+
+        graf.addVertex("Hologram");
+        graf.addVertex("Kostum");
+        graf.addVertex("Pengepasan kostum");
+        graf.addVertex("Kontak tim lain");
+        graf.addVertex("Pembuatan panggung");
+        graf.addVertex("Persiapan panggung");
+        graf.addVertex("Pencahayaan");
+        graf.addVertex("Properti panggung");
+
+        graf.addEdge("Hologram", "Kostum");
+        graf.addEdge("Kostum", "Pengepasan kostum");
+        graf.addEdge("Kostum", "Pembuatan panggung");
+        graf.addEdge("Kostum", "Pencahayaan");
+        graf.addEdge("Kontak tim lain", "Pengepasan kostum");
+        graf.addEdge("Kontak tim lain", "Persiapan panggung");
+        graf.addEdge("Persiapan panggung", "Pembuatan panggung");
+        graf.addEdge("Properti panggung", "Kostum");
+        graf.addEdge("Properti panggung", "Persiapan panggung");
+
+       System.out.println(graf.topologicalSort());
     }
 }
