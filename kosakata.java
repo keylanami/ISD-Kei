91c4b1dcfe155537398fb4c834f80237dd5384b9
klyroe <keylaanmrr@gmail.com>
2025-06-07 23:58:14 +0700

    C, Dictionary

---
+++ kosakata.java
@@ -0,0 +1,18 @@
+public class kosakata {
+    String kata;
+    String arti;
+
+    public kosakata(String kata, String arti) {
+        this.kata = kata;
+        this.arti = arti;
+    }
+
+    public String getKata() {
+        return kata;
+    }
+
+    public String getArti() {
+        return arti;
+    }
+}
+
